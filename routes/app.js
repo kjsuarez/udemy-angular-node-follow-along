9c00e94b239346679a0624c5d0baf6108e0e33f9
Kevin Suarez <kevinSuarez@Kevins-Air.fios-router.home>
2018-05-22 13:25:38 -0400

    added a post route that passes data via url

--- routes/app.js
+++ routes/app.js
@@ -5,8 +5,13 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
-router.get('/message', function (req, res, next) {
-  res.render('a_node_view', {message: "I'm passed in from the router!"});
+router.get('/message/:msg', function (req, res, next) {
+  res.render('a_node_view', {message: req.params.msg});
+})
+
+router.post('/message', function (req,res,next) {
+  var message =  req.body.message;
+  res.redirect('/message/' + message);
 })
 
 module.exports = router;
