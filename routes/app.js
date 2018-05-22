94c45d4c4cdbe347bb403ff40bf26fe58de32da6
Kevin Suarez <kevinSuarez@Kevins-Air.fios-router.home>
2018-05-22 13:07:02 -0400

    added a baby route on the back end

--- routes/app.js
+++ routes/app.js
@@ -5,4 +5,8 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
+router.get('/message', function (req, res, next) {
+  res.render('a_node_view', {message: "I'm passed in from the router!"});
+})
+
 module.exports = router;
