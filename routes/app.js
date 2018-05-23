0eceb7a0e0845aae2536854c49493d9620d3ebab
Kevin Suarez <kjsuarez94@gmail.com>
2018-05-23 16:02:33 -0400

    successfully made post request to mongodb

--- routes/app.js
+++ routes/app.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    res.render('a_node_view');
 });
 
-router.get('/message/:msg', function (req, res, next) {
-  res.render('a_node_view', {message: req.params.msg});
-})
 
-router.post('/message', function (req,res,next) {
-  var message =  req.body.message;
-  res.redirect('/message/' + message);
+router.post('/', function (req,res,next) {
+  var email = req.body.email
+  var user = new User({
+    firstName: "some",
+    lastName: "guy",
+    email: email,
+    password: "soVerySecret"
+  });
+  user.save();
+  res.redirect('/');
 })
 
 module.exports = router;
