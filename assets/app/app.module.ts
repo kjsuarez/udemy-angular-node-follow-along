4f5548c47e54e8870b05d89b46855d1aa87d70e8
Kevin Suarez <kjsuarez94@gmail.com>
2018-06-07 17:08:29 -0700

    built a reactive form for auth/signup

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from "./app.component";
 
@@ -30,7 +30,7 @@ import { routing } from "./app.routing";
         LogoutComponent,
         HeaderComponent
     ],
-    imports: [BrowserModule, FormsModule, routing],
+    imports: [BrowserModule, FormsModule, ReactiveFormsModule, routing],
     bootstrap: [AppComponent]
 })
 export class AppModule {
--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'auth-signup',
   templateUrl: './signup.component.html'
 })
 
-export class SignupComponent {
+export class SignupComponent implements onInit {
+  signupForm:  formGroup;
 
+  onSubmit(){
+    console.log(this.signupForm);
+    this.signupForm.reset();
+  }
+
+  ngOnInit(){
+    this.signupForm = new FormGroup({
+      firstName: new FormControl(null, Validators.required),
+      lastName: new FormControl(null, Validators.required),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
+      ]),
+      password: new FormControl(null, Validators.required)
+
+    });
+  }
 }
