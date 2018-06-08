7ee4474601939652c301e186257531476ec93d22
Kevin Suarez <kjsuarez94@gmail.com>
2018-06-07 17:22:25 -0700

    repeat signup form for login

--- assets/app/auth/login.component.ts
+++ assets/app/auth/login.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'auth-login',
   templateUrl:'./login.component.html'
 })
 
-export class LoginComponent {
+export class LoginComponent implements onInit {
 
+  loginForm:  formGroup;
+
+  onSubmit(){
+    console.log(this.loginForm);
+    this.loginForm.reset();
+  }
+
+  ngOnInit(){
+    this.loginForm = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
+      ]),
+      password: new FormControl(null, Validators.required)
+
+    });
+  }
 }
